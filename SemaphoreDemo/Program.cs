7afba8df65d13426c106030b5a927560436e4bf2
Harry <364424187@qq.com>
2023-01-08 10:39:02 +0800

    add semaphoredemo

---
+++ SemaphoreDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿HttpClient _client = new HttpClient
+{
+    Timeout = TimeSpan.FromSeconds(5)
+};
+SemaphoreSlim _gate = new SemaphoreSlim(20);
+
+
+
+Task.WaitAll(CreateCalls().ToArray());
+
+
+IEnumerable<Task> CreateCalls()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        yield return CallGoogle();
+    }
+}
+
+
+async Task CallGoogle()
+{
+    try
+    {
+        await _gate.WaitAsync();
+        var response = await _client.GetAsync("https://google.com");
+        _gate.Release();
+
+        Console.WriteLine(response.StatusCode);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);    
+    }
+}
