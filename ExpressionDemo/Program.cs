d19e0782096b340cd420ae0fddf114506b311927
Harry <364424187@qq.com>
2023-01-10 23:09:38 +0800

    add expression

---
+++ ExpressionDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+var nArgument = Expression.Parameter(typeof(int), "n");
+var result = Expression.Variable(typeof(int), "result");
+
+// Creating a label that represents the return value
+LabelTarget label = Expression.Label(typeof(int));
+
+var initializeResult = Expression.Assign(result, Expression.Constant(1));
+
+// This is the inner block that performs the multiplication,
+// and decrements the value of 'n'
+var block = Expression.Block(
+    Expression.Assign(result,
+        Expression.Multiply(result, nArgument)),
+    Expression.PostDecrementAssign(nArgument)
+);
+
+// Creating a method body.
+BlockExpression body = Expression.Block(
+    new[] { result },
+    initializeResult,
+    Expression.Loop(
+        Expression.IfThenElse(
+            Expression.GreaterThan(nArgument, Expression.Constant(1)),
+            block,
+            Expression.Break(label, result)
+        ),
+        label
+    )
+);
