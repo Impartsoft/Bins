3757f47a71128352a64a7d646356c2af09105c56
Harry <364424187@qq.com>
2023-02-14 20:31:57 +0800

    Create Practise.cs

---
+++ ILDemo/Practise.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILDemo
+{
+    internal class Practise
+    {
+        static MethodInfo METHOD_INFO = typeof(Console).GetMethod(nameof(Console.Write), new Type[] { typeof(string) });
+
+        public static void DynamicMethodMyTest1()
+        {
+
+            var dm = new DynamicMethod("HelloWord", null, null);
+            var il = dm.GetILGenerator();
+
+            il.Emit(OpCodes.Ldstr, "HelloWorld");
+            //il.Emit(OpCodes.Call,METHOD_INFO);
+            il.EmitCall(OpCodes.Call, METHOD_INFO, null);
+
+            il.Emit(OpCodes.Ret);
+
+            var delegate1 = dm.CreateDelegate(typeof(Action));
+            delegate1.DynamicInvoke();
+        }
+
+        public static void ExpressionMyTest1()
+        {
+
+            var value = Expression.Constant("HelloWorld");
+            var eb = Expression.Call(METHOD_INFO, value);
+            var ep = Expression.Lambda<Action>(eb);
+
+            var delegate2 = ep.Compile();
+            delegate2.DynamicInvoke();
+        }
+    }
+}
