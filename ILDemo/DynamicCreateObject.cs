3bdeb22f42dbe02f271e98b1d02573fcd736119a
Harry <364424187@qq.com>
2023-03-09 22:41:50 +0800

    Create DynamicCreateObject.cs

---
+++ ILDemo/DynamicCreateObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILDemo
+{
+    internal class DynamicCreateObject
+    {
+        public void Test()
+        {
+            ModuleBuilder mymodule = AssemblyBuilder.DefineDynamicAssembly(new System.Reflection.AssemblyName("Test"), AssemblyBuilderAccess.Run).DefineDynamicModule(nameof(mymodule));
+
+            var type = mymodule.DefineType("MyType");
+            type.DefineField();
+        }
+    }
+}
