b519598193011ec41fd2c9e40629a77f8a460da8
Harry <364424187@qq.com>
2023-03-13 21:43:03 +0800

    Create KafkaDemo

---
+++ KafkaDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Hello, World!");
+
+
+var conf = new ConsumerConfig
+{
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "host1:9092",
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    var result = await producer.ProduceAsync("weblog", new Message<Null, string> { Value = "a log message" });
+
+    producer.Flush(TimeSpan.FromSeconds(10));
+}
+
+
+
+
+
