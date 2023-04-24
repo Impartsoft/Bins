3dfced8e173609fe4c8107cd9b36359c123cf55a
Harry <364424187@qq.com>
2023-04-24 22:33:28 +0800

    update async test

--- ThreadDemo/ThreadDemo/File.cs
+++ ThreadDemo/ThreadDemo/File.cs
@@ -3,31 +3,31 @@ using System.Linq;
 
 namespace ThreadDemo
 {
-    public class File
-    {
-        public string Path { get; set; }
-        public IEnumerable<File> Children { get; set; }
-    }
+    //public class File
+    //{
+    //    public string Path { get; set; }
+    //    public IEnumerable<File> Children { get; set; }
+    //}
 
-    public class File2 : File
-    {
-    }
+    //public class File2 : File
+    //{
+    //}
 
-    public class File3 : File
-    {
-    }
+    //public class File3 : File
+    //{
+    //}
 
-    public static class FileExtentions
-    {
-        public static IEnumerable<File> GetFiles(this File file)
-        {
-            if (file.Children == null || !file.Children.Any())
-                yield return file;
+    //public static class FileExtentions
+    //{
+    //    public static IEnumerable<File> GetFiles(this File file)
+    //    {
+    //        if (file.Children == null || !file.Children.Any())
+    //            yield return file;
 
-            foreach (var c in file.Children)
-            {
-                c.GetFiles();
-            }
-        }
-    }
+    //        foreach (var c in file.Children)
+    //        {
+    //            c.GetFiles();
+    //        }
+    //    }
+    //}
 }
--- ThreadDemo/ThreadDemo/Program.cs
+++ ThreadDemo/ThreadDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,54 +10,61 @@ namespace ThreadDemo
     {
         static async Task Main(string[] args)
         {
-            //await AsyncTest2.Test();
+            
+           //await AsyncTest2.Test();
 
-           await AsyncLocalTest.AsyncLocalTest2();
-           await AsyncLocalTest.AsyncLocalTest3();
+           //await AsyncLocalTest.AsyncLocalTest2();
+           //await AsyncLocalTest.AsyncLocalTest3();
         }
 
-        private static async Task AsyncAwaitTest()
+        static async Task summonDogLocallyTask()
         {
-            Console.WriteLine("开始");
-
-            File[] cs = new File[2];
-            cs[0] = new File { Path = "path1" };
-            cs[1] = new File { Path = "path2" };
-            var file = new File();
-            file.Children = cs;
-
-            var files = file.GetFiles();
-
-            foreach (var tfile in files)
-                Console.WriteLine(tfile.Path);
-
-            Console.WriteLine("结束");
-
-            File2[] cs2 = new File2[2];
-            cs2[0] = new File2 { Path = "path21" };
-            cs2[1] = new File2 { Path = "path22" };
-            var file2 = new File2();
-            file2.Children = cs2;
-
-            var file2s = file2.GetFiles();
-            foreach (var tfile in file2s)
-                Console.WriteLine(tfile.Path);
-
-            // dosomething
-            Console.WriteLine("方法一、当前线程ID:" + Thread.CurrentThread.ManagedThreadId);
-            await DoSomething();
-            Console.WriteLine("方法二、当前线程ID:" + Thread.CurrentThread.ManagedThreadId);
-            // dosomething
-            Console.ReadLine();
+            Console.WriteLine("1.");
+            string dogText = await File.ReadAllTextAsync("x");
         }
 
-        private async static Task DoSomething()
-        {
-            await Task.Run(() =>
-            {
-                Console.WriteLine("方法三、当前线程ID:" + Thread.CurrentThread.ManagedThreadId);
-            });
-        }
+        //private static async Task AsyncAwaitTest()
+        //{
+        //    Console.WriteLine("开始");
+
+        //    File[] cs = new File[2];
+        //    cs[0] = new File { Path = "path1" };
+        //    cs[1] = new File { Path = "path2" };
+        //    var file = new File();
+        //    file.Children = cs;
+
+        //    var files = file.GetFiles();
+
+        //    foreach (var tfile in files)
+        //        Console.WriteLine(tfile.Path);
+
+        //    Console.WriteLine("结束");
+
+        //    File2[] cs2 = new File2[2];
+        //    cs2[0] = new File2 { Path = "path21" };
+        //    cs2[1] = new File2 { Path = "path22" };
+        //    var file2 = new File2();
+        //    file2.Children = cs2;
+
+        //    var file2s = file2.GetFiles();
+        //    foreach (var tfile in file2s)
+        //        Console.WriteLine(tfile.Path);
+
+        //    // dosomething
+        //    Console.WriteLine("方法一、当前线程ID:" + Thread.CurrentThread.ManagedThreadId);
+        //    await DoSomething();
+        //    Console.WriteLine("方法二、当前线程ID:" + Thread.CurrentThread.ManagedThreadId);
+        //    // dosomething
+        //    Console.ReadLine();
+        //}
+
+        //private async static Task DoSomething()
+        //{
+        //    await Task.Run(() =>
+        //    {
+        //        Console.WriteLine("方法三、当前线程ID:" + Thread.CurrentThread.ManagedThreadId);
+        //    });
+        //}
     }
 
     public static class AwaitAnythingExtentions
